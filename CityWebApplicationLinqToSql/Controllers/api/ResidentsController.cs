8f71319d8ec1bd816ef985ada44bcf9f598e2c9f
matan-ysayas <matan.ysayas@gmail.com>
2022-01-06 12:18:33 +0200

    Add project files.

---
+++ CityWebApplicationLinqToSql/Controllers/api/ResidentsController.cs
@@ -0,0 +1,46 @@
+﻿using CityWebApplicationLinqToSql.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+
+
+namespace CityWebApplicationLinqToSql.Controllers
+{
+    public class ResidentsController : ApiController
+    {
+        static string connecitonString = "Data Source=LAPTOP-E49VKATT;Initial Catalog=City;Integrated Security=True;Pooling=False";
+        DataResidentsDataContext CityDB = new DataResidentsDataContext(connecitonString);
+        // GET: api/Residents
+        public IHttpActionResult Get()
+        {
+            
+            return Ok(CityDB.Residents.ToList());
+        }
+
+        // GET: api/Residents/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Residents
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Residents/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Residents/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ CityWebApplicationLinqToSql/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CityWebApplicationLinqToSql.MvcApplication" Language="C#" %>
